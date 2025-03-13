43c81c6a8f5b65366ad9a275bf284b43e00ebb46
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2025-03-14 01:35:40 +0400

    set correct page based on route

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { SidebarItemRoute, SidenavItem } from '../../models/sidebar.model';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-main',
@@ -11,6 +12,9 @@ import { SidebarItemRoute, SidenavItem } from '../../models/sidebar.model';
   imports: [SidenavComponent, RouterModule],
 })
 export class MainComponent implements OnInit {
+  private usersService = inject(UsersService);
+  private router = inject(Router);
+
   selectedPage = signal<SidebarItemRoute>(SidebarItemRoute.OVERVIEW);
 
   sidenavItems = signal<SidenavItem[]>([
@@ -43,5 +47,13 @@ export class MainComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  setSelectedPage() {
+    const currentUrl = this.router.url.substring(1);
+    this.selectedPage.set(currentUrl as SidebarItemRoute);
+  }
+
+  ngOnInit() {
+    this.setSelectedPage();
+    this.usersService.getCurrentUser().subscribe();
+  }
 }
