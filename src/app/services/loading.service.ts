ad4eb114d33cc659cfbb5ed6a5ac637a41e34703
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2025-03-27 23:16:22 +0400

    add loading service

---
+++ src/app/services/loading.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingService {
+  requestCount = 0;
+  constructor(private spinnerService: NgxSpinnerService) {}
+
+  loading() {
+    this.requestCount++;
+    this.spinnerService.show();
+  }
+
+  idle() {
+    this.requestCount--;
+    if (this.requestCount <= 0) {
+      this.requestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
